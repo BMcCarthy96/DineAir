13f1d19cf126c559a9f992f78e0ce8d445f888e1
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-04-29 09:20:48 -0400

    Added initial LoginFormPage/css

---
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch } from 'react-redux';
+// import { useModal } from '../../context/Modal';
+import './LoginFormPage.css';
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+//   const { closeModal } = useModal();
+
+  const resetForm = () => {
+    setCredential("");
+    setPassword("");
+    setErrors({});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({});
+    return dispatch(sessionActions.login({ credential, password }))
+      .then(() => {
+        resetForm();
+        // closeModal()
+      })
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data?.errors?.credential) {
+          setErrors({ credential: data.errors.credential });
+        } else {
+          setErrors({ credential: "The provided credentials were invalid." });
+        }
+      });
+  };
+
+  // Reset the form on unmount
+  useEffect(() => {
+    return () => resetForm();
+  }, []);
+
+  return (
+    <div className="login-modal-container">
+      <h1>Log In</h1>
+
+      {errors.credential && (
+        <div className="error-container">
+          <p>{errors.credential}</p>
+        </div>
+      )}
+
+      <form onSubmit={handleSubmit} id="login-form">
+        <div className="email">
+          <input
+            type="text"
+            value={credential}
+            placeholder="Username or Email"
+            onChange={(e) => setCredential(e.target.value)}
+          />
+        </div>
+
+        <div className="password">
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="log-in-button-div">
+          <button
+            disabled={credential.length < 4 || password.length < 6}
+            className="login-button"
+            type="submit"
+          >
+            Log In
+          </button>
+        </div>
+      </form>
+
+      <div className="demo-user-div">
+        <button
+          type="button"
+          className="demo-user-button"
+          onClick={() => {
+            setCredential("FakeUser1");
+            setPassword("password2");
+            setTimeout(() => {
+              document.getElementById("login-form").requestSubmit();
+            }, 0);
+          }}
+        >
+          Demo User
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default LoginFormPage;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,7 @@
+// restore session user thunk action
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
