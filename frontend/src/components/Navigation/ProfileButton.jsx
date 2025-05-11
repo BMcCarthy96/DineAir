3f817f00618cd42c4e42cf53892cf7f2deca64d3
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-05-11 11:31:56 -0400

    Fixed dark/light mode theme for buttons, need to fix theme toggle icon and navbar transparency on dark mode (should stay fixed and solid like light mode

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,125 +1,142 @@
-import { useState, useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import * as sessionActions from '../../store/session';
+import { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import * as sessionActions from "../../store/session";
 import { HiBars3 } from "react-icons/hi2";
 import { PiReceiptFill } from "react-icons/pi";
 import { IoIosHeart } from "react-icons/io";
-import './ProfileButton.css';
+import { RiRestaurantLine } from "react-icons/ri";
+import "./ProfileButton.css";
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [showSideBar, setSideBar] = useState(false);
-  const ulRef = useRef();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [showSideBar, setSideBar] = useState(false);
+    const ulRef = useRef();
 
-  const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
-    setSideBar(!showSideBar);
-  };
-
-  useEffect(() => {
-    if (!showSideBar) return;
-
-    const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
-        setSideBar(false);
-      }
+    const toggleMenu = (e) => {
+        e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+        setSideBar(!showSideBar);
     };
 
-    document.addEventListener('click', closeMenu);
+    useEffect(() => {
+        if (!showSideBar) return;
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showSideBar]);
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) {
+                setSideBar(false);
+            }
+        };
 
-  const closeMenu = () => setSideBar(false);
+        document.addEventListener("click", closeMenu);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-    closeMenu();
-    navigate('/'); // Navigates to home page after logging out
-  };
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showSideBar]);
 
-  const getRestaurantsLink = () => {
-    if (user.userType === "admin") {
-      return "/restaurants/admin"; // AdminRestaurantsPage
-    } else if (user.userType === "restaurantOwner") {
-      return "/restaurants/owner"; // OwnerRestaurantsPage
-    }
-    return null;
-  };
+    const closeMenu = () => setSideBar(false);
 
-  const ulClassName = "profile-dropdown" + (showSideBar ? "" : " hidden");
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+        closeMenu();
+        navigate("/"); // Navigates to home page after logging out
+    };
 
-  return (
-    <>
-      <button onClick={toggleMenu} className="profile-button">
-        <HiBars3 size={30} />
-      </button>
+    const getRestaurantsLink = () => {
+        if (user.userType === "admin") {
+            return "/restaurants/admin"; // AdminRestaurantsPage
+        } else if (user.userType === "restaurantOwner") {
+            return "/restaurants/owner"; // OwnerRestaurantsPage
+        }
+        return null;
+    };
 
-      <ul className={ulClassName} ref={ulRef}>
-        {user ? (
-          <>
-            <div className="options">
-              <div>Hello, {user.firstName}</div>
-              <div>{user.email}</div>
-            </div>
-            <hr />
-            <div className="manage-div">
-              <div>
-                <Link to="/orders" className="manage-link"> <PiReceiptFill size={20} className="icon-padding"/>
-                  Orders
-                </Link>
-              </div>
-              <div>
-                <Link to="/favorites" className="manage-link"> <IoIosHeart size={20} className="icon-padding"/>
-                  Favorites
-                </Link>
-              </div>
-              {(user.userType === "admin" || user.userType === "restaurantOwner") && (
-                <div>
-                  <Link
-                    to={getRestaurantsLink()}
-                    className="manage-link"
-                    onClick={closeMenu}
-                  >
-                    Restaurants
-                  </Link>
-                </div>
-              )}
-              {user.userType === "runner" && (
-                <div>
-                  <Link
-                    to="/runner-dashboard"
-                    className="manage-link"
-                    onClick={closeMenu}
-                  >
-                    Runner Dashboard
-                  </Link>
-                </div>
-              )}
-            </div>
-            <hr />
-            <div className="logout-button-div">
-              <button className="logout-button" onClick={logout}>
-                Log Out
-              </button>
-            </div>
-          </>
-        ) : (
-          <div className="sidebar-content">
-            <Link to="/login" className="sidebar-link">
-              Log In
-            </Link>
-            <Link to="/signup" className="sidebar-link">
-              Sign Up
-            </Link>
-          </div>
-        )}
-      </ul>
-    </>
-  );
+    const ulClassName = "profile-dropdown" + (showSideBar ? "" : " hidden");
+
+    return (
+        <>
+            <button onClick={toggleMenu} className="profile-button">
+                <HiBars3 size={30} />
+            </button>
+
+            <ul className={ulClassName} ref={ulRef}>
+                {user ? (
+                    <>
+                        <div className="options">
+                            <div>Hello, {user.firstName}</div>
+                            <div>{user.email}</div>
+                        </div>
+                        <hr />
+                        <div className="manage-div">
+                            <div>
+                                <Link to="/orders" className="manage-link">
+                                    {" "}
+                                    <PiReceiptFill
+                                        size={20}
+                                        className="icon-padding"
+                                    />
+                                    Orders
+                                </Link>
+                            </div>
+                            <div>
+                                <Link to="/favorites" className="manage-link">
+                                    {" "}
+                                    <IoIosHeart
+                                        size={20}
+                                        className="icon-padding"
+                                    />
+                                    Favorites
+                                </Link>
+                            </div>
+                            {(user.userType === "admin" ||
+                                user.userType === "restaurantOwner") && (
+                                <div>
+                                    <Link
+                                        to={getRestaurantsLink()}
+                                        className="manage-link"
+                                        onClick={closeMenu}
+                                    >
+                                        {" "}
+                                        <RiRestaurantLine
+                                            size={20}
+                                            className="icon-padding"
+                                        />
+                                        Restaurants
+                                    </Link>
+                                </div>
+                            )}
+                            {user.userType === "runner" && (
+                                <div>
+                                    <Link
+                                        to="/runner-dashboard"
+                                        className="manage-link"
+                                        onClick={closeMenu}
+                                    >
+                                        Runner Dashboard
+                                    </Link>
+                                </div>
+                            )}
+                        </div>
+                        <hr />
+                        <div className="logout-button-div">
+                            <button className="logout-button" onClick={logout}>
+                                Log Out
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <div className="sidebar-content">
+                        <Link to="/login" className="sidebar-link">
+                            Log In
+                        </Link>
+                        <Link to="/signup" className="sidebar-link">
+                            Sign Up
+                        </Link>
+                    </div>
+                )}
+            </ul>
+        </>
+    );
 }
 
 export default ProfileButton;
